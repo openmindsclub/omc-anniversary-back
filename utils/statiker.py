47847a73706de426e923fb0314717279aaa636e7
zaqks <imzakbro@gmail.com>
2024-02-10 04:17:30 +0100

    staticker

---
+++ utils/statiker.py
@@ -0,0 +1,27 @@
+html = "/home/zak/Desktop/myDesk/djangoProjects/omc-anniversary-back/userApp/templates/userApp/index.html"
+
+lines = []
+result = []
+
+with open(html, "r") as f:
+    lines = f.readlines()
+    f.close()
+
+current = ""
+for i in lines:
+    current = i
+
+    if current.__contains__("src="):
+        # src="{% static 'userApp/' %}"
+        
+        # src="{% static 'userApp/
+        # ' %}"
+        pass
+    
+    result.append(current)
+    
+
+
+with open("result.html", "w") as f:
+    f.writelines(result)
+    f.close()
